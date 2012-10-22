c2ce73547ec72df5c6562683b20b28c6d13c75f4
Caoimhe Chaos <caoimhechaos@protonmail.com>
2012-10-23 00:44:01 +0200

    Add a simple program to run a NOP on a Doozer cluster.

---
+++ ping.cc
@@ -0,0 +1,74 @@
+/*-
+ * Copyright (c) 2012 Caoimhe Chaos <caoimhechaos@protonmail.com>,
+ *                    Ancient Solutions. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions  of source code must retain  the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions  in   binary  form  must   reproduce  the  above
+ *    copyright  notice, this  list  of conditions  and the  following
+ *    disclaimer in the  documentation and/or other materials provided
+ *    with the distribution.
+ *
+ * THIS  SOFTWARE IS  PROVIDED BY  ANCIENT SOLUTIONS  AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO,  THE IMPLIED WARRANTIES OF  MERCHANTABILITY AND FITNESS
+ * FOR A  PARTICULAR PURPOSE  ARE DISCLAIMED.  IN  NO EVENT  SHALL THE
+ * FOUNDATION  OR CONTRIBUTORS  BE  LIABLE FOR  ANY DIRECT,  INDIRECT,
+ * INCIDENTAL,   SPECIAL,    EXEMPLARY,   OR   CONSEQUENTIAL   DAMAGES
+ * (INCLUDING, BUT NOT LIMITED  TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE,  DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT  LIABILITY,  OR  TORT  (INCLUDING NEGLIGENCE  OR  OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif /* HAVE_CONFIG_H */
+#include <QtCore/QString>
+#include <QtCore/QScopedPointer>
+#include <glog/logging.h>
+
+#include <iostream>
+#include <string>
+
+#include "doozer.h"
+
+using doozer::Conn;
+using doozer::Error;
+
+int main(int argc, char** argv)
+{
+	google::InitGoogleLogging(argv[0]);
+
+	// Get default parameters from the environment.
+	QScopedPointer<Conn> c(new Conn());
+	Error* err;
+
+	if (!c->IsValid())
+	{
+		err = c->GetError();
+		LOG(ERROR) << "Unable to connect to Doozer: "
+			<< err->ToString();
+		delete err;
+		return 1;
+	}
+
+	err = c->Nop();
+	if (err)
+	{
+		LOG(ERROR) << "Unable to connect to Doozer: "
+			<< err->ToString();
+		delete err;
+		return 1;
+	}
+	else
+		LOG(INFO) << "Ping successful.";
+
+	return 0;
+}
